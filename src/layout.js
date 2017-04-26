8cf49e92322cde0c0a528307a6c9d317a07f8635
Nguyễn Minh Hoàng (Tom Kazansky) <nmhoang05@gmail.com>
2017-04-26 10:54:14 +0700

    column + row layout

--- src/layout.js
+++ src/layout.js
@@ -1,5 +1,18 @@
 window.onload = function() {
     var theBoard = $("#board");
+
+    var helpers = {
+        find_list_node_with_name: function(name) {
+            var nds = theBoard.find('> .list-wrapper');
+            var h2;
+            for(var i = 0; i < nds.length; i++) {
+                h2 = $(nds[i]).find('h2.list-header-name-assist');
+                if (h2.html() == name) return nds[i];
+            }
+            return null;
+        }
+    };
+
     chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
         if (!msg || !msg.source) return;
         
@@ -7,11 +20,11 @@ window.onload = function() {
             case "chrome-trello-layout-init":
                 chrome.extension.sendRequest({
                     source: 'chrome-trello-layout-init',
-                    url: location.href
+                    url: location.href,
+                    choice: choice
                 });
                 break;
             case "chrome-trello-layout-choice":
-                localStorage.setItem('chrome-trello-layout.choice', msg.choice);
                 switch(msg.choice) {
                     case '0':
                         defaultLayout();
@@ -20,56 +33,147 @@ window.onload = function() {
                         gridLayout();
                         break;
                     case '2':
-                        columnLayout(msg.data);
-                        break;
                     case '3':
-                        rowLayout(msg.data);
+                        layoutDataFetch();
                         break;
                 }
                 break;
+            case "chrome-trello-layout-apply":
+                localStorage.setItem('chrome-trello-layout.choice', msg.choice);
+                switch(msg.choice) {
+                    case '2':
+                        theBoard.removeClass('chrome-trello-layout-grid');
+                        theBoard.addClass('chrome-trello-layout-column');
+                        theBoard.removeClass('chrome-trello-layout-row');
+                        break;
+                    case '3':
+                        theBoard.removeClass('chrome-trello-layout-grid');
+                        theBoard.removeClass('chrome-trello-layout-column');
+                        theBoard.addClass('chrome-trello-layout-row');
+                        break;
+                }
+                choice = msg.choice;
+                localStorage.setItem('chrome-trello-layout.count', msg.count);
+                localStorage.setItem('chrome-trello-layout.format', JSON.stringify(msg.list));
+                layoutApply(msg);
+                break;
         }
     });
+    
+    function getHeadersList() {
+        var list = [];
+        var h2s = $('h2.list-header-name-assist');
+        for(var i = 0; i < h2s.length; i++) {
+            list.push(h2s[i].innerHTML);
+        }
+        return list;
+    }
 
     function defaultLayout() {
+        if (theBoard.find('> .chrome-trello-group').length > 0) {
+            var nds = theBoard.find('.list-wrapper');
+
+            for(var i = 0; i < nds.length; i++) {
+                theBoard.append(nds[i]);
+            }
+            theBoard.find('> .chrome-trello-group').remove();
+        }
+        localStorage.setItem('chrome-trello-layout.choice', 0);
+        choice = 0;
         theBoard.removeClass('chrome-trello-layout-grid');
         theBoard.removeClass('chrome-trello-layout-column');
         theBoard.removeClass('chrome-trello-layout-row');
     }
 
     function gridLayout() {
+        if (theBoard.find('> .chrome-trello-group').length > 0) {
+            var nds = theBoard.find('.list-wrapper');
+
+            for(var i = 0; i < nds.length; i++) {
+                theBoard.append(nds[i]);
+            }
+            theBoard.find('> .chrome-trello-group').remove();
+        }
+        localStorage.setItem('chrome-trello-layout.choice', 1);
+        choice = 1;
         theBoard.addClass('chrome-trello-layout-grid');
         theBoard.removeClass('chrome-trello-layout-column');
         theBoard.removeClass('chrome-trello-layout-row');
     }
 
-    function columntLayout(data) {
-        theBoard.removeClass('chrome-trello-layout-grid');
-        theBoard.addClass('chrome-trello-layout-column');
-        theBoard.removeClass('chrome-trello-layout-row');
+    function layoutDataFetch() {
+        chrome.extension.sendRequest({
+            source: 'chrome-trello-layout-fetch',
+            choice: choice,
+            count: localStorage.getItem('chrome-trello-layout.count') || 1,
+            list: getHeadersList()
+        });
     }
 
-    function rowLayout(data) {
-        theBoard.removeClass('chrome-trello-layout-grid');
-        theBoard.removeClass('chrome-trello-layout-column');
-        theBoard.addClass('chrome-trello-layout-row');
+    function layoutApply(data) {
+        // remove old groups
+        var nds = theBoard.find('.list-wrapper');
+
+        for(var i = 0; i < nds.length; i++) {
+            theBoard.append(nds[i]);
+        }
+        theBoard.find('> .chrome-trello-group').remove();
+
+        if (data.extra == 0) {
+            theBoard.addClass('shared-scroll');
+        } else {
+            theBoard.removeClass('shared-scroll');
+        }
+
+        var grps = [];
+        var dimension = Math.floor(100.0 / data.count);
+        var dimension_name = choice == 2 ? 'width' : 'min-height';
+
+        for(var i = 0; i < data.count; i++) {
+            grps[i] = $('<div class="chrome-trello-group u-fancy-scrollbar"></div>');
+            grps[i].appendTo(theBoard);
+
+            grps[i].css(dimension_name, dimension + '%');
+        }
+
+        var add_list = $('.js-add-list');
+        for(i = 0; i < data.list.length; i++) {
+            for(var j = 0; j < data.list[i].length; j++) {
+                if (data.list[i][j] == '<b>[New List]</b>') {
+                    grps[i].append(add_list[0]);
+                } else {
+                    var nd = helpers.find_list_node_with_name(data.list[i][j]);
+                    if (nd != null) {
+                        grps[i].append(nd);
+                    }
+                }
+            }
+        }
+
+        var nds = theBoard.find('> .list-wrapper');
+        for(var i = 0; i < nds.length; i++) {
+            grps[0].append(nds[i]);
+        }
     }
-    
+
     var choice = localStorage.getItem('chrome-trello-layout.choice') || 0;
+    var init_obj = {
+        source: 'chrome-trello-layout-init',
+        url: location.href,
+        choice: choice
+    };
     switch(choice) {
         case '1':
             gridLayout();
             break;
         case '2':
-            columnLayout(msg.data);
+            init_obj.list = getHeadersList();
             break;
         case '3':
-            rowLayout(msg.data);
+            init_obj.list = getHeadersList();
             break;
     }
     
-    chrome.extension.sendRequest({
-        source: 'chrome-trello-layout-init',
-        url: location.href
-    });
+    chrome.extension.sendRequest(init_obj);
 };
 
--- src/popup_script.js
+++ src/popup_script.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+    var choice = 0;
+    var layout_choice_box = null;
+    var layout_count = 0;
+    var full_item_list;
+    var item_list;
     var helpers = {
         from_same_domain: function(refer) {
           if (refer.length == 0) return false;
@@ -7,8 +12,63 @@ window.onload = function() {
           if (i == -1) return false;
           ref = ref.substring(0, i);
           return ref == "trello.com";
+        },
+        get_text_of_node: function(node, level) {
+            var i, result, text, child;
+            result = '';
+            for (i = 0; i < node.childNodes.length; i++) {
+                child = node.childNodes[i];
+                text = null;
+                if (child.nodeType === 1 && level > 0) {
+                    text = getTextFromNode(child, level - 1);
+                } else if (child.nodeType === 3) {
+                    text = child.nodeValue;
+                }
+                if (text) {
+                    result += text;
+                }
+            }
+            return result;
+        },
+        is_last_node_br: function(node) {
+            if (node.childNodes.length > 0) {
+                return node.childNodes[node.childNodes.length - 1].tagName == 'BR';
+            }
+            return false;
+        },
+        remove_last_br: function(node) {
+            if (node.childNodes.length > 0) {
+                var child = node.childNodes[node.childNodes.length - 1];
+                if (child.tagName == 'BR') {
+                    child.remove();
+                }
+            }
+        },
+        select_after_last_node: function(node) {
+            if (node.childNodes.length == 0) return;
+            var child = node.childNodes[node.childNodes.length - 1]
+            var selection = window.getSelection();
+            var range = document.createRange();
+            range.setStartAfter(child);
+            range.setEndAfter(child);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        },
+        intersection: function(){
+            return Array.from(arguments).reduce(function(previous, current){
+                return previous.filter(function(element){
+                    return current.indexOf(element) > -1;
+                });
+            });
+        },
+        difference: function(){
+            return Array.from(arguments).reduce(function(previous, current){
+                return previous.filter(function(element){
+                    return current.indexOf(element) == -1;
+                });
+            });
         }
-    }
+    };
 
     chrome.extension.onRequest.addListener(receiveMessage);
 
@@ -27,34 +87,250 @@ window.onload = function() {
                 if (helpers.from_same_domain(data.url)) {
                     $('#init-mask').remove();
                     $('#extension-content').css('display', 'block');
+                    choice = data.choice;
+                    var rdo = $('input[name=layout-choice][value=' + choice + ']');
+                    prev_option = rdo.parent();
+                    prev_option.attr('data-state', 'active');
+                    rdo.prop('checked', true);
+                    
+                    layout_choice_box = prev_option.siblings('.content');
+                    
+                    switch (choice) {
+                        case '2':
+                        case '3':
+                            init_layout_box(data);
+                            break;
+                    }
+                }
+                break;
+            case 'chrome-trello-layout-fetch':
+                layout_choice_box = prev_option.siblings('.content');
+                switch (choice) {
+                    case '2':
+                    case '3':
+                        init_layout_box(data);
+                        break;
                 }
                 break;
         }
     }
 
-    var choice = localStorage.getItem('chrome-trello-layout.choice') || 0;
     var prev_option = null;
     $('input[name=layout-choice]').each(function() {
         var inst = $(this);
         var wrp = inst.parent();
-        if (inst.val() == choice) {
-            inst.prop('checked', true);
-            wrp.attr('data-state', 'active');
-            prev_option = wrp;
-        }
 
         inst.on('click', function() {
             if (prev_option != null) prev_option.removeAttr('data-state');
             wrp.attr('data-state', 'active');
             prev_option = wrp;
 
-            localStorage.setItem('chrome-trello-layout.choice', inst.val());
+            choice = inst.val();
             sendMessage({
                 source: 'chrome-trello-layout-choice',
-                choice: inst.val()
+                choice: choice
             });
         });
     });
+
+    var markup_header = ['<div class="column-header">',
+        'Count: <input type="number" id="count-input" min="1" max="5" /> (from 1 ~ 5)<br/>',
+        '<label class="option"><input type="checkbox" name="individual-column-scroll" /> Individual scroll for each column</label>',
+    '</div><br/>'];
+    var markup_header_r = ['<div class="column-header">',
+        'Count: <input type="number" id="count-input" min="1" max="5" /> (from 1 ~ 5)<br/>',
+        '<label class="option"><input type="checkbox" name="individual-column-scroll" /> Individual scroll for each row</label>',
+        '</div><br/>'];
+
+    var markup_list = ['<div class="item-list-wrapper">',
+        '<div class="item-list" contenteditable="plaintext-only"></div>',
+        '<div class="item-auto-list"></div>',
+    '</div>'];
+    var markup_footer = '<br/><div><button type="button" id="btn-apply">Apply</button></div>';
+    
+    function init_layout_box(data) {
+        full_item_list = data.list;
+        full_item_list.push('<b>[New List]</b>');
+        item_list = full_item_list.slice();
+        layout_count = parseInt(data.count);
+
+        layout_choice_box.html('');
+
+        var header = $((choice == 2 ? markup_header : markup_header_r).join(''));
+        header.appendTo(layout_choice_box);
+        var count_input = header.find('#count-input');
+        count_input.val(layout_count);
+        count_input.on('input', function() {
+            layout_count = Math.max( Math.min(parseInt(count_input.val()), 5), 1);
+            populate_inputs();
+        });
+
+        var lists = [];
+        var ls;
+        for (var i = 0; i < layout_count; i++) {
+            ls = $(markup_list.join(''));
+            ls.appendTo(layout_choice_box);
+            handle_autolist(ls);
+            lists.push(ls);
+        }
+        var footer = $(markup_footer);
+        footer.appendTo(layout_choice_box);
+        footer.find('#btn-apply').click(apply_layout);
+
+        function populate_inputs() {
+            item_list = full_item_list.slice();
+            header.detach();
+            footer.detach();
+
+            layout_choice_box.html('');
+
+            header.appendTo(layout_choice_box);
+
+            var lists = [];
+            var ls;
+            for (var i = 0; i < layout_count; i++) {
+                ls = $(markup_list.join(''));
+                ls.appendTo(layout_choice_box);
+                handle_autolist(ls);
+                lists.push(ls);
+            }
+
+            footer.appendTo(layout_choice_box);
+        }
+    }
+
+    function handle_autolist(inst) {
+        var auto_list = inst.find('.item-auto-list');
+        var input = inst.find('.item-list');
+        var input_nd = input[0];
+        var timer_blur = null;
+
+        function get_current_list() {
+            var result = [];
+            var list = input.find('.item');
+            for (var i = 0; i < list.length; i++) {
+                result.push(list[i].innerHTML);
+            }
+            return result;
+        }
+
+        function populate_list() {
+            auto_list.html('');
+            var it;
+            for (var i = 0; i < item_list.length; i++) {
+                it = $('<span class="item">' + item_list[i] + "</span>");
+                it.appendTo(auto_list);
+                it.click(pick_item);
+            }
+
+            it = $('<span class="item no-item">no match</span>');
+            it.appendTo(auto_list);
+        }
+
+        function scan() {
+            var txt = helpers.get_text_of_node(input_nd, 0).toLowerCase();
+            var match_count = 0;
+            var items = auto_list.find('.item:not(.no-item)');
+            var cont;
+            for( var i = 0; i < items.length; i++) {
+                cont = items[i].innerText.toLowerCase();
+                if (cont.indexOf(txt) > -1 || txt.indexOf(cont) > -1) {
+                    $(items[i]).removeClass('not-match').addClass('match');
+                    match_count++;
+                } else {
+                    $(items[i]).removeClass('match').addClass('not-match');
+                }
+            }
+            if (match_count == 0) {
+                auto_list.attr('data-empty', 'true');
+            } else {
+                auto_list.removeAttr('data-empty');
+            }
+        }
+
+        function pick_item() {
+            if (timer_blur != null) {
+                input.focus();
+                clearTimeout(timer_blur);
+                timer_blur = null;
+            }
+            var val = this.innerHTML;
+            $(this).remove();
+
+            item_list.splice( item_list.indexOf(val), 1 );
+            helpers.remove_last_br(input_nd);
+            $('<span class="item" contenteditable="false">' + val + '</span>').prependTo(input);
+            if (!helpers.is_last_node_br(input_nd)) $('<br>').appendTo(input);
+
+            helpers.select_after_last_node(input_nd);
+        }
+
+        input.focus(function() {
+            if (timer_blur != null) return;
+
+            populate_list();
+            auto_list.css('display', 'block');
+            scan();
+        });
+
+        input.keydown(function(e) {
+            switch (e.which) {
+                case 8:
+                case 46:
+                    var bef_list = get_current_list();
+                    setTimeout(function() {
+                        var aft_list = get_current_list();
+
+                        var diff = helpers.difference(bef_list, aft_list);
+                        item_list = diff.concat(item_list);
+                        item_list.sort(function(a, b) { return a.indexOf('<b>') > -1});
+                        populate_list();
+
+                        if (!helpers.is_last_node_br(input_nd)) $('<br>').appendTo(input);
+
+                        scan();
+                    }, 100);
+                    break;
+                case 13:
+                    e.preventDefault();
+                    break;
+                default:
+                    setTimeout(scan, 25);
+                    break;
+            }
+        });
+
+        input.blur(function() {
+            if (timer_blur != null) clearTimeout(timer_blur);
+            timer_blur = setTimeout(function() {
+                timer_blur = null;
+
+                auto_list.css('display', 'none');
+            }, 150);
+        });
+    }
+
+    function apply_layout() {
+        var lists = [];
+        var lst;
+        var inp_lst = layout_choice_box.find('.item-list');
+        var items;
+        for (var i = 0; i < inp_lst.length; i++) {
+            lst = [];
+            items = $(inp_lst[i]).find('.item');
+            for(var j = 0; j < items.length; j++) {
+                lst.push(items[j].innerHTML);
+            }
+            lists.push(lst);
+        }
+        sendMessage({
+            source: 'chrome-trello-layout-apply',
+            choice: choice,
+            count: layout_count,
+            extra: $('input[name=individual-column-scroll]').is(':checked') ? 1 : 0,
+            list: lists
+        });
+    }
     
     sendMessage({
         source: 'chrome-trello-layout-init'
